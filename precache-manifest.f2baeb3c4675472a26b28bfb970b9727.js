fd651734536bb0df960f63981885ae976e271d8f
Andrzej Stasinski <andis_as@wp.pl>
2020-11-22 19:39:19 +0100

    Updates

---
+++ precache-manifest.f2baeb3c4675472a26b28bfb970b9727.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55574ba0376eff260832cb90ccf475da",
+    "url": "/paint/index.html"
+  },
+  {
+    "revision": "51ec521f99c26900fdae",
+    "url": "/paint/static/js/2.79e2d494.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/paint/static/js/2.79e2d494.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d78f573bac7c0e393920",
+    "url": "/paint/static/js/main.86fa804a.chunk.js"
+  },
+  {
+    "revision": "f490d405e2b1903f4700",
+    "url": "/paint/static/js/runtime-main.e430cf85.js"
+  }
+]);
